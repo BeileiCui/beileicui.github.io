a0b358560ce69d6c6c6921d2005dc18126026725
BeileiCui <114715678+BeileiCui@users.noreply.github.com>
2026-04-16 06:08:03 +0000

    Deploying to gh-pages from @ BeileiCui/beileicui.github.io@4b696abdf01633d9bd3fd088554ff17d986e7570 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,7 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%62%65%69%6C%65%69%63%75%69@%6C%69%6E%6B.%63%75%68%6B.%65%64%75.%68%6B", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -539,7 +539,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=G-WO0oYAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
