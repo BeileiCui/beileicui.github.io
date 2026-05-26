4ee363b6aafd81c8060903335934f06d96e34a56
BeileiCui <114715678+BeileiCui@users.noreply.github.com>
2026-05-26 06:13:37 +0000

    Deploying to gh-pages from @ BeileiCui/beileicui.github.io@72aab8cc1d0fb0b23d04065edc5576139529d973 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -110,12 +110,7 @@ ninja.data = [{
           section: "News",},{id: "news-two-papers-endogsim-and-coge-are-early-accepted-by-miccai-2026",
           title: 'Two papers, EndoGSim and CoGE, are early accepted by MICCAI 2026!',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
+          section: "News",},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
